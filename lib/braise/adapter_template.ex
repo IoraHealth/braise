74a5cb02969397bfca3c53fa3902e24bfa6a9d1c
Patrick Robertson <patricksrobertson@gmail.com>
2015-01-28 11:11:07 -0500

    Use a template string, and a sane default.

--- lib/braise/adapter_template.ex
+++ lib/braise/adapter_template.ex
@@ -1,34 +1,42 @@
 defmodule Braise.AdapterTemplate do
-  def generate_from_resource(resource = %Braise.Resource{}) do
+  @template_string """
+  import DS from 'ember-data';
+  import Ember from 'ember';
+
+  export default DS.RESTAdapter.extend({
+    host: "<%= scheme %>://<%= host %>",
+    namespace: "<%= path %>",
+    token: Ember.computed.alias('accessTokenWrapper.token'),
+
+    headers: function() {
+      return {
+        'AUTHORIZATION': 'Bearer ' + this.get('token');
+      };
+    }.property('token')
+  });
+  """
+
+  def generate_from_resource(resource, template_string \\ @template_string)
+
+  def generate_from_resource(resource = %Braise.Resource{}, template_string) do
     case Braise.Resource.url(resource) do
-      {:ok, url}    -> template(url)
+      {:ok, url}    -> fill_template(url, template_string) |> ok_tuple
       {:error, msg} -> {:error, msg}
     end
   end
 
-  def generate_from_resource(_) do
+  def generate_from_resource(_, _) do
     { :error, "Invalid JSON Schema" }
   end
 
 
-  defp template(%{host: host, path: path, scheme: scheme}) do
-    file_string = """
-    import DS from 'ember-data';
-    import Ember from 'ember';
-    
-    export default DS.RESTAdapter.extend({
-      host: "#{scheme}://#{host}",
-      namespace: "#{path}",
-      token: Ember.computed.alias('accessTokenWrapper.token'),
+  defp fill_template(%{host: host, path: path, scheme: scheme}, template_string) do
+    String.replace(template_string, ~r/<%= scheme %>/, scheme)
+    |> String.replace(~r/<%= host %>/, host)
+    |> String.replace(~r/<%= path %>/, path)
+  end
 
-      headers: function() {
-        return {
-          'AUTHORIZATION': 'Bearer ' + this.get('token');
-        };
-      }.property('token')
-    });
-    """
-
-    { :ok, file_string }
+  defp ok_tuple(string) do
+    {:ok, string}
   end
 end
