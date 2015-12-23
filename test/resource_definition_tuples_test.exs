0358a3c4be73f0d6132bf36b220294b489bfd01f
Alex Rothenberg <alex@alexrothenberg.com>
2015-12-23 11:52:06 -0500

    Some parts of the resource response are inline and not $ref
    This is ok and we handle by leaving the inline portions and only replacing the $ref portions
    
    Works with our medication_verification.json sample now
    
    Issue #12

--- test/resource_definition_tuples_test.exs
+++ test/resource_definition_tuples_test.exs
@@ -9,11 +9,10 @@ defmodule ResourceDefinitionTuplesTest do
                  "first_name" => %{"$ref" => "#/definitions/patient/definitions/first_name"},
                  "guid" =>       %{"$ref" => "#/definitions/patient/definitions/pirate"}}
 
+    list_of_references = Dict.to_list(response)
     expected_result = [{"dob", "dob"}, {"email", "email"}, {"first_name", "first_name"}, {"guid", "pirate"}]
 
-    resource = %Braise.Resource{response: response}
-
-    assert expected_result == map(resource)
+    assert expected_result == map(list_of_references)
   end
 
 end
