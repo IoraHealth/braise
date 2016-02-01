53d86312f74d648fa689180b260a976f7a465e19
Patrick Robertson <patricksrobertson@gmail.com>
2016-02-01 11:05:29 -0500

    Release 0.3.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Braise.Mixfile do
 
   def project do
     [app: :braise,
-     version: "0.1.0",
+     version: "0.3.0",
      elixir: "~> 1.0",
      escript: escript_config,
      description: "A library that converts JSON Schema into ember models/adapters.",
